790607dca8fbf28568474515bc65d01c0feca6a8
melckzedeck <melckzedeck063@gmail.com>
2023-05-04 11:49:09 +0300

    modified  auth controller

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -4,7 +4,7 @@ const {promisify } = require('util')
 const jwt = require('jsonwebtoken')
 const {User} = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync')
-const AppError = require('./../utils/appError')
+const AppError = require('./../utils/AppError')
 
 
 const  sendResponse = (statusCode, message, res, data) => {
